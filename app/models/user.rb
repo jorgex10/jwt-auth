9ab641a231794dec6a255c76e72b85abd5558dc9
Jorge Calvo G. <jorgeecg10@gmail.com>
2023-12-27 18:07:23 -0500

    Adding JTI to user model and jwt config for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 end
